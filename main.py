596b778ebb588723a5ee69b13bac945166317158
Tiger Nie <nhl0819@gmail.com>
2020-04-04 18:22:15 -0500

    adding print statements

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import time
 import RPi.GPIO as GPIO
+import logging
+
 import covid_api
 from lcd import gpio_init, lcd_init, lcd_print
 
@@ -8,15 +10,19 @@ API_UPDATE_INTERVAL = 60 * 5
 
 def main():
     # Initialise GPIO pins
+    print("Initialising GPIO")
     gpio_init()
 
     # Initialise display
+    print("Initialising LCD")
     lcd_init()
 
+    print("Getting covid data")
     covid_data = covid_api.CovidData.from_worldometer_country()
     last_update = time.time()
 
     while True:
+        print("Printing data on LCD display")
         lcd_print(
             "US Cases:{:7}".format(covid_data.cases),
             "US Today:{:7}".format(covid_data.today),
@@ -31,6 +37,7 @@ def main():
 
         new_time = time.time()
         if new_time - last_update > API_UPDATE_INTERVAL:
+            print("Getting covid data")
             covid_data.update()
             last_update = new_time
         else:
