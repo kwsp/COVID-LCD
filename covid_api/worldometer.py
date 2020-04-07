a44b4cd0a73eda4a7296c9d01ad844c73af1d4aa
Tiger Nie <nhl0819@gmail.com>
2020-04-07 14:19:38 -0500

    commented out bs4 code for scrapping worldometer because parsing the entire worldometer page on a raspberrypi zero w is way too slow

--- covid_api/worldometer.py
+++ covid_api/worldometer.py
@@ -1,7 +1,7 @@
 from typing import NamedTuple
 
-import bs4
-from bs4 import BeautifulSoup
+# import bs4
+# from bs4 import BeautifulSoup
 import requests
 
 
@@ -11,25 +11,25 @@ class CovidData(NamedTuple):
     deaths: str
     recovered: str
 
-    @classmethod
-    def from_worldometer_country(cls, country: str = "USA") -> "CovidData":
+    # @classmethod
+    # def from_worldometer_country(cls, country: str = "USA") -> "CovidData":
 
-        try:
-            row = get_row(country)
-            country = row.td
-            cases = country.find_next("td")
-            today = cases.find_next("td")
-            deaths = today.find_next("td")
-            recovered = deaths.find_next("td").find_next("td")
-        except Exception as e:
-            raise ValueError("Parsing of worldometer country data failed: {}".format(e))
+    # try:
+    # row = get_row(country)
+    # country = row.td
+    # cases = country.find_next("td")
+    # today = cases.find_next("td")
+    # deaths = today.find_next("td")
+    # recovered = deaths.find_next("td").find_next("td")
+    # except Exception as e:
+    # raise ValueError("Parsing of worldometer country data failed: {}".format(e))
 
-        return cls(
-            cases=cases.string.strip(),
-            today=today.string.strip(),
-            deaths=deaths.string.strip(),
-            recovered=recovered.string.strip(),
-        )
+    # return cls(
+    # cases=cases.string.strip(),
+    # today=today.string.strip(),
+    # deaths=deaths.string.strip(),
+    # recovered=recovered.string.strip(),
+    # )
 
     @classmethod
     def from_api(cls, country: str = "USA") -> "CovidData":
@@ -46,35 +46,34 @@ class CovidData(NamedTuple):
         )
 
 
+# def parse_wordometer() -> bs4.element.ResultSet:
+# """
+# Parse the worldometer covid 19 page
+# Return the data table in soup format
+# """
+# worldometer_page = requests.get("https://www.worldometers.info/coronavirus/")
 
-def parse_wordometer() -> bs4.element.ResultSet:
-    """
-    Parse the worldometer covid 19 page
-    Return the data table in soup format
-    """
-    worldometer_page = requests.get("https://www.worldometers.info/coronavirus/")
+# soup = BeautifulSoup(worldometer_page.text, "html.parser")
 
-    soup = BeautifulSoup(worldometer_page.text, "html.parser")
+# # Find all table rows
+# rows = soup.find_all("tr")
 
-    # Find all table rows
-    rows = soup.find_all("tr")
-
-    return rows
-
-
-def get_row(country: str = "USA") -> bs4.element.Tag:
-    """
-    Find the USA row from the table
-    """
-    rows = parse_wordometer()
-
-    for row in rows[1:]:
-        if row.td.string == country:
-            return row
-
-    raise ValueError("Country name not found")
+# return rows
 
 
-if __name__ == "__main__":
-    data = CovidData.from_worldometer_country()
-    breakpoint()
+# def get_row(country: str = "USA") -> bs4.element.Tag:
+# """
+# Find the USA row from the table
+# """
+# rows = parse_wordometer()
+
+# for row in rows[1:]:
+# if row.td.string == country:
+# return row
+
+# raise ValueError("Country name not found")
+
+
+# if __name__ == "__main__":
+# data = CovidData.from_worldometer_country()
+# breakpoint()
